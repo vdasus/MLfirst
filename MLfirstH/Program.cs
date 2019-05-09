335499813442f4f2f668d9e25e7a3d5f37697d69
vdasus <me@vdasus.com>
2019-05-09 19:57:47 +0300

    first play

--- MLfirstH/Program.cs
+++ MLfirstH/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Microsoft.ML;
+using MLfirstHML.Model.DataModels;
 
 namespace MLfirstH
 {
@@ -6,7 +8,27 @@ namespace MLfirstH
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                var res = Console.ReadLine();
+                Console.WriteLine(ConsumeModel(res));
+            }
+        }
+
+        public static string ConsumeModel(string inp)
+        {
+            // Load the model
+            MLContext mlContext = new MLContext();
+            ITransformer mlModel = mlContext.Model.Load("MLModel.zip", out var modelInputSchema);
+            var predEngine = mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(mlModel);
+
+            // Use the code below to add input data
+            var input = new ModelInput();
+            input.Comment = inp;
+
+            // Try model on sample data
+            ModelOutput result = predEngine.Predict(input);
+            return $"{result.Prediction} - {result.Score}";
         }
     }
 }
